090fbf1945d6d66ae6b885741acb5fffb084e16e
astaseen <taseen@iembsys.com>
2015-07-15 14:18:04 +0530

    Removed folder from repository

--- ReportPhpBackend/hourly_report/delhi/send_hourly_mail_halt.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/php
-cd /mnt/itrack/ReportPhpBackend/hourly_report/delhi
-php mail_hourly_halt_report.php
-
-
-
--- ReportPhpBackend/hourly_report/nagpur/send_hourly_mail_halt.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/php
-cd cd /mnt/itrack/ReportPhpBackend/hourly_report/nagpur
-php mail_hourly_halt_report.php
-
-
-
